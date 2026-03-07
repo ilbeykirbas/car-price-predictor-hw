c1fa4783e5103d1d2bf082506c932fb92de0785f
Erdem <erdemyy13@gmail.com>
2026-03-07 20:49:44 +0300

    rename 2

--- src/metrics.py
+++ src/metrics.py
@@ -1,37 +1,38 @@
 import numpy as np
 
 class Metrics:
-    def mserror(self, y_true, y_pred):
-        y_true = np.array(y_true).reshape(-1)
+    def mserror(self, y, y_pred):
+        y = np.array(y).reshape(-1)
         y_pred = np.array(y_pred).reshape(-1)
-        mse = np.mean((y_true - y_pred) ** 2)
+        mse = np.mean((y - y_pred) ** 2)
         return mse
 
-    def maerror(self, y_true, y_pred):
-        y_true = np.array(y_true).reshape(-1)
+    def maerror(self, y, y_pred):
+        y = np.array(y).reshape(-1)
         y_pred = np.array(y_pred).reshape(-1)
-        mae = np.mean(np.abs(y_true - y_pred))
+        mae = np.mean(np.abs(y - y_pred))
         return mae
 
-    def rmserror(self, y_true, y_pred):
-        mse = self.mean_squared_error(y_true, y_pred)
+    def rmserror(self, y, y_pred):
+        mse = self.mean_squared_error(y, y_pred)
         rmse = np.sqrt(mse)
         return rmse
 
-    def r_squared(self, y_true, y_pred):
-        y_true = np.array(y_true).reshape(-1)
+    def r_squared(self, y, y_pred):
+        y = np.array(y).reshape(-1)
         y_pred = np.array(y_pred).reshape(-1)
-        y_mean = np.mean(y_true)
-        ss_res = np.sum((y_true - y_pred) ** 2)
-        ss_tot = np.sum((y_true - y_mean) ** 2)
+        y_mean = np.mean(y)
+        ss_res = np.sum((y - y_pred) ** 2)
+        ss_tot = np.sum((y - y_mean) ** 2)
         r2 = 1 - (ss_res / ss_tot)
         return r2
 
     def summary(self, y_true, y_pred):
         results = {
-            "MSE": self.mean_squared_error(y_true, y_pred),
-            "MAE": self.mean_absolute_error(y_true, y_pred),
-            "RMSE": self.root_mean_squared_error(y_true, y_pred),
-            "R2 Score": self.r_squared(y_true, y_pred)
+            "MSE": self.mean_squared_error(y, y_pred),
+            "MAE": self.mean_absolute_error(y, y_pred),
+            "RMSE": self.root_mean_squared_error(y, y_pred),
+            "R2 Score": self.r_squared(y, y_pred)
         }
         return results
+
