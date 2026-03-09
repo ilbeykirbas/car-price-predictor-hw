b74b247a6edc15ee52198081bdb73353f0fde1ab
Erdem <erdemyy13@gmail.com>
2026-03-09 12:40:21 +0300

    Convert training data to NumPy arrays before fitting

--- src/train.py
+++ src/train.py
@@ -1,6 +1,12 @@
 from src.preprocessing import run_preprocessing
 
 def train_model(model, X_train, y_train_scaled):
-    model = model.fit(X_train, y_train_scaled)
+    X_train_numpy=X_train.to_numpy()
+    y_train_numpy=y_train_scaled.to_numpy()
+       
+    model.fit(X_train_numpy,y_train_numpy)
 
+
+
+   
     return model
